6d655eaf6c4a70a2e8b93f2528733b257f3e6e33
Evan Hiroshige <hiroshige.e@husky.neu.edu>
2020-02-25 00:10:44 -0500

    Fix bugs

--- src/DailyScheduleFetcher.py
+++ src/DailyScheduleFetcher.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from Booking import Booking
-from datetime import datetime
+from datetime import datetime, timedelta
 import pytz
 
 NY_TIMEZONE = pytz.timezone('America/New_York')
@@ -26,7 +26,7 @@ class DailyScheduleFetcher:
   def get_daily_schedule(self, daily_elements, day_of_month):
     for day_element in daily_elements:
       date_element = day_element.find_element_by_class_name('calendar-event')
-      print(date_element.text, day_of_month)
+
       if (date_element.text == day_of_month):
         events = day_element.find_elements_by_class_name('booking-event')
         return self.parse_events_to_daily_schedule(events)
@@ -37,9 +37,8 @@ class DailyScheduleFetcher:
       event_name = event.find_element_by_xpath('.//div[1]/span').text
       event_time_start = event.find_element_by_xpath('.//div[2]/span[1]').text
       event_time_end = event.find_element_by_xpath('.//div[2]/span[2]').text
-
-      print(event_name + " from " + event_time_start + " " + event_time_end)
       bookings.append(self.build_booking(event_name, event_time_start, event_time_end))
+      print(event_name + " from " + event_time_start + " " + event_time_end)
     return bookings
 
   def build_booking(self, event_name, event_time_start, event_time_end):
@@ -53,10 +52,11 @@ class DailyScheduleFetcher:
     start_time = datetime_NY.strptime(event_time_start, '%I:%M%p')
     start_date_time = datetime_NY.combine(start_date, start_time.time())
 
-    if start_date_time.hour >= 12:
-      end_date = datetime_NY.combine(end_date, datetime.time(24, 0, 0))
     end_time = datetime_NY.strptime(event_time_end, '- %I:%M%p')
     end_date_time = datetime_NY.combine(end_date, end_time.time())
+    if start_date_time.hour >= 12:
+      tomorrow = datetime.now() + timedelta(days=1)
+      end_date_time = datetime_NY.combine(tomorrow.date(), end_time.time())
 
     return (start_date_time, end_date_time)
 
